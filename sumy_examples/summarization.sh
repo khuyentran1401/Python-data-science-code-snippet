02acff44a839160ef51edbd88ed58354803fec09
khuyentran1401 <khuyentran1476@gmail.com>
2021-03-10 11:24:32 -0600

    fix sumy txt

--- sumy_examples/summarization.sh
+++ sumy_examples/summarization.sh
@@ -3,17 +3,17 @@
 echo "Summarize text from URL: https://www.dataquest.io/blog/learn-data-science/"
 
 echo "Summazing text using LexRank..."
-sumy lex-rank --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ >> lex-rank.txt 
+sumy lex-rank --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ | tee lex-rank.txt 
 echo "Done"
 echo "Summazing text using Luhn’s Heuristic Method ..."
-sumy luhn --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ >> luhn.txt 
+sumy luhn --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ | tee luhn.txt 
 echo "Done"
 echo "Summazing text using Edmundson Heuristic Method ..."
-sumy edmundson --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ >> edmundson.txt 
+sumy edmundson --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ | tee> edmundson.txt 
 echo "Done"
 echo "Summazing text using Latent Semantic Analysis ..."
-sumy lsa --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ >> lsa.txt 
+sumy lsa --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ | tee lsa.txt 
 echo "Done"
 echo "Summazing text using TextRank ..."
-sumy text-rank --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ >> text-rank.txt 
+sumy text-rank --length=10 --url=https://www.dataquest.io/blog/learn-data-science/ | tee text-rank.txt 
 echo "Done"
