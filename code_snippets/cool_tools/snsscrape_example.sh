4c75a10552f9c6f7a6481f1be6c725f95de5170d
khuyentran1401 <khuyentran1476@gmail.com>
2021-06-30 08:25:49 -0500

    add snsscrape

---
+++ code_snippets/cool_tools/snsscrape_example.sh
@@ -0,0 +1,8 @@
+# Scrape all tweets from @KhuyenTran16
+snscrape twitter-user KhuyenTran16
+
+# Save outputs
+snscrape twitter-user KhuyenTran16 >> khuyen_tweets 
+
+# Scrape 100 tweets with hashtag python
+snscrape --max-results 100 twitter-hashtag python
