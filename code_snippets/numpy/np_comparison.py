a428f2afb5b1ef613118a8c1ea882ddf02a5acdc
khuyentran1401 <khuyentran1476@gmail.com>
2021-07-15 08:27:53 -0500

    add np comparison

--- code_snippets/numpy/np_comparison.py
+++ code_snippets/numpy/np_comparison.py
@@ -1,13 +1,19 @@
 import numpy as np
 
 a = np.array([1, 2, 3])
+b = np.array([4, 1, 2])
 
-print(a <= 2)
+print(a < 2)
 """
 [ True False False]
 """
 
-print(a[a <= 2])
+print(a < b)
 """
-[1 2]
+[ True False False]
+"""
+
+print(a[a < b])
+"""
+[1]
 """
