b5e6c57e8925e4664ab185c067d4419551f0d708
Khuyen <khuyentran1476@gmail.com>
2021-07-31 08:28:41 -0500

    add np where

--- code_snippets/pandas/pd_Series_map.py
+++ code_snippets/pandas/pd_Series_map.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-s = pd.Series(['a', 'b', 'c'])
+s = pd.Series(["a", "b", "c"])
 
-print(s.map({'a': 1, 'b': 2, 'c': 3}))
+print(s.map({"a": 1, "b": 2, "c": 3}))
+"""
+0    1
+1    2
+2    3
+dtype: int64
+"""
