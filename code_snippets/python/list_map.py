b33f750ab7a7c9becb9604d239833e972d579b05
Khuyen <khuyentran1476@gmail.com>
2021-08-20 09:31:54 -0500

    add df_fillna

--- code_snippets/python/list_map.py
+++ code_snippets/python/list_map.py
@@ -1,7 +1,9 @@
 nums = [1, 2, 3]
-print(list(map(str, nums))) # ['1', '2', '3']
+print(list(map(str, nums)))  # ['1', '2', '3']
+
 
 def multiply_by_two(num: float):
     return num * 2
 
-print(list(map(multiply_by_two, nums))) # [2, 4, 6]
+
+print(list(map(multiply_by_two, nums)))  # [2, 4, 6]
