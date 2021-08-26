2f604c58f18e4aba5c72accff0aa28e8a5e183b3
khuyentran1401 <khuyentran1476@gmail.com>
2021-08-26 08:38:32 -0500

    add set difference

--- code_snippets/pandas/pd_dataframe_agg.py
+++ code_snippets/pandas/pd_dataframe_agg.py
@@ -1,10 +1,11 @@
-import pandas as pd
-import numpy as np
 from collections import Counter
+
+import pandas as pd
+
 
 def count_two(nums: list):
     return Counter(nums)[2]
 
-df = pd.DataFrame({'coll': [1, 3, 5],
-                    'col2': [2, 4, 6]})
-print(df.agg(['sum', count_two]))
+
+df = pd.DataFrame({"coll": [1, 3, 5], "col2": [2, 4, 6]})
+print(df.agg(["sum", count_two]))
