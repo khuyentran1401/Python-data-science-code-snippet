f4cd2f6e3c6ecd754affa356c5806fc67c65349d
khuyentran1401 <khuyentran1476@gmail.com>
2021-09-08 08:22:01 -0500

    add distfit

---
+++ code_snippets/data_science_tools/distfit_example.py
@@ -0,0 +1,11 @@
+import numpy as np
+from distfit import distfit
+
+X = np.random.normal(0, 3, 1000)
+
+# Initialize model
+dist = distfit()
+
+# Find best theoretical distribution for empirical data X
+dist.fit_transform(X)
+dist.plot()
