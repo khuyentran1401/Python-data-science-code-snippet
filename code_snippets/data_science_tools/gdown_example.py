949a47c938a13b78ed4d8bb01f0b5e71c77ac38b
Khuyen Tran <khuyentran1476@gmail.com>
2021-09-11 14:48:56 -0500

    Update gdown_example.py

--- code_snippets/data_science_tools/gdown_example.py
+++ code_snippets/data_science_tools/gdown_example.py
@@ -2,7 +2,7 @@
 import gdown
 
 # Format of url: https://drive.google.com/uc?id=YOURFILEID
-url = 'https://drive. google. com/uc?id=1jI1cmxqnwsmC-vbl8dNY6b4aNBtBbKy3'
+url = 'https://drive.google.com/uc?id=1jI1cmxqnwsmC-vbl8dNY6b4aNBtBbKy3'
 output = 'Twitter.zip'
 
 gdown.download(url, output, quiet=False)
